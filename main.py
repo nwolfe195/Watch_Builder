22c932ad24b52f62257acbc31ee7adbcfb738855
nwolfe195 <nicholaswolfe195@gmail.com>
2021-08-25 15:44:52 -0700

    Fixed region display.  Added regions file.

--- main.py
+++ main.py
@@ -1,74 +1,48 @@
 import tkinter as tk
 from tkinter import *
-from tkinter import ttk
 import os
 import pandas as pd
 
 
 class Watch_Builder_GUI:
     def __init__(self, master):
+        pd.set_option('display.max_colwidth', 1000)
         self.master = master
-        master.title('Watch Builder')
+        self.master.geometry('400x600')
+        self.master.title('Watch Builder')
 
-        self.region_file = 'regions.txt'
-        self.region_header = ['Name', 'Description', 'CR', 'Foraging', 'Navigation']
+        region_file = 'regions.csv'
+        self.regions_df = pd.read_csv(region_file)
+        self.description_text = StringVar()
+        self.description_text.set('Select a region')
+        self.cr_text = StringVar()
+        self.foraging_text = StringVar()
+        self.navigation_text = StringVar()
 
-        self.tab_control = ttk.Notebook(root)
-        self.tab_watches = ttk.Frame(self.tab_control)
-        self.tab_tables = ttk.Frame(self.tab_control)
-        self.tab_control.add(self.tab_watches, text='Watch')
-        self.tab_control.add(self.tab_tables, text='Tables')
-        self.tab_control.pack(expand=1, fill='both')
-        Label(self.tab_watches, text='This is where each watch will be calculated').grid(column=0, row=0, padx=30,
-                                                                                         pady=30)
-        Button(self.tab_watches, text='Clear Tables', command=self.Clear_Data).grid(column=0, row=1, padx=30, pady=30)
+        self.set_components()
 
-        self.Display_Regions()
+    def set_components(self):
+        regions = self.regions_df['Name'].tolist()
+        region_variable = StringVar(self.master)
+        OptionMenu(self.master, region_variable, *regions, command=self.select_region).grid(row=0, column=0)
+        Label(textvariable=self.description_text, wraplength=400).grid(row=1, column=0, columnspan=6)
+        Label(text='CR').grid(row=2, column=0)
+        Label(textvariable=self.cr_text).grid(row=2, column=1)
+        Label(text='Foraging DC').grid(row=2, column=2)
+        Label(textvariable=self.foraging_text).grid(row=2, column=3)
+        Label(text='Navigation DC').grid(row=2, column=4)
+        Label(textvariable=self.navigation_text).grid(row=2, column=5)
 
-    def Clear_Data(self):
-        os.remove(self.region_file)
-
-    def Display_Regions(self):
-        Label(self.tab_tables, text='Regions')
-
-        for i in range(len(self.region_header)):
-            Label(self.tab_tables, text=self.region_header[i]).grid(row=1, column=i)
-
-        if os.path.isfile(self.region_file):
-            regions_df = pd.read_csv(self.region_file)
-            rows = regions_df.shape[0]
-            columns = regions_df.shape[1]
-            for x in range(rows):
-                for y in range(columns):
-                    Label(self.tab_tables, text=regions_df.iloc[x, y]).grid(row=x + 2, column=y)
-        else:
-            rows = 1
-
-        new_name = Entry(self.tab_tables)
-        new_name.grid(row=rows + 3, column=0)
-        new_description = Entry(self.tab_tables)
-        new_description.grid(row=rows + 3, column=1)
-        new_cr = Entry(self.tab_tables)
-        new_cr.grid(row=rows + 3, column=2)
-        new_foraging = Entry(self.tab_tables)
-        new_foraging.grid(row=rows + 3, column=3)
-        new_navigation = Entry(self.tab_tables)
-        new_navigation.grid(row=rows + 3, column=4)
-        Button(self.tab_tables, text='Add Region', command=lambda: self.Add_Region(new_name.get(), new_description.get(),
-                                                                                   new_cr.get(), new_foraging.get(),
-                                                                                   new_navigation.get())).grid(row=rows + 3, column=5)
-
-    def Add_Region(self, name, description, cr, foraging, navigation):
-        if os.path.isfile(self.region_file):
-            regions_df = pd.read_csv(self.region_file)
-        else:
-            regions_df = pd.DataFrame(columns=self.region_header)
-
-        new_region = {'Name': name, 'Description': description, 'CR': cr, 'Foraging': foraging,
-                      'Navivation': navigation}
-        regions_df = regions_df.append(new_region, ignore_index=True)
-
-        regions_df.to_csv(self.region_file, index=False)
+    def select_region(self, selected):
+        region_data = self.regions_df.loc[self.regions_df['Name'] == selected]
+        description = region_data['Description'].to_string(index=False)
+        self.description_text.set(description)
+        cr = region_data['CR'].to_string(index=False)
+        self.cr_text.set(cr)
+        foraging = region_data['Foraging'].to_string(index=False)
+        self.foraging_text.set(foraging)
+        navigation = region_data['Navigation'].to_string(index=False)
+        self.navigation_text.set(navigation)
 
 
 root = Tk()
