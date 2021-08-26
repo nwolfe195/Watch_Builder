3c4c690933cd76f0d3d10225e4bf5c60e37f341f
nwolfe195 <nicholaswolfe195@gmail.com>
2021-08-25 17:19:12 -0700

    Encounter roller started.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import tkinter as tk
 from tkinter import *
-import os
+from tkinter.messagebox import showinfo
 import pandas as pd
+import random
 
 
 class Watch_Builder_GUI:
     def __init__(self, master):
         pd.set_option('display.max_colwidth', 1000)
         self.master = master
-        self.master.geometry('400x600')
+        self.master.geometry('600x600')
         self.master.title('Watch Builder')
 
         region_file = 'regions.csv'
@@ -18,20 +18,69 @@ class Watch_Builder_GUI:
         self.cr_text = StringVar()
         self.foraging_text = StringVar()
         self.navigation_text = StringVar()
+        self.encounter_chance_text = StringVar()
+        self.region_variable = StringVar()
+
+        encounter_file = 'encounters.csv'
+        self.encounters_df = pd.read_csv(encounter_file)
+        self.encounter_variable = StringVar()
+        self.encounter_variable.set('100')
+
+        self.encounter_text = StringVar()
+        self.rain_text = StringVar()
+        self.wind_text = StringVar()
 
         self.set_components()
 
     def set_components(self):
         regions = self.regions_df['Name'].tolist()
-        region_variable = StringVar(self.master)
-        OptionMenu(self.master, region_variable, *regions, command=self.select_region).grid(row=0, column=0)
-        Label(textvariable=self.description_text, wraplength=400).grid(row=1, column=0, columnspan=6)
+        OptionMenu(self.master, self.region_variable, *regions, command=self.select_region).grid(row=0, column=0)
+        Label(textvariable=self.description_text, wraplength=600).grid(row=1, column=0, columnspan=6)
         Label(text='CR').grid(row=2, column=0)
         Label(textvariable=self.cr_text).grid(row=2, column=1)
         Label(text='Foraging DC').grid(row=2, column=2)
         Label(textvariable=self.foraging_text).grid(row=2, column=3)
-        Label(text='Navigation DC').grid(row=2, column=4)
+        Label(text='Navigation DC',).grid(row=2, column=4)
         Label(textvariable=self.navigation_text).grid(row=2, column=5)
+
+        Label(text='Encounter').grid(row=3, column=0)
+        encounter_modifiers = [-20, -10, 0, 10, 20, 30, 40, 50]
+        OptionMenu(self.master, self.encounter_variable, *encounter_modifiers).grid(row=3, column=1)
+
+        Button(text='Travel Watch', command=self.travel_watch).grid(row=4, column=0)
+        Button(text='Activity Watch', command=self.activity_watch).grid(row=4, column=2)
+        Button(text='Sleep Watch', command=self.sleep_watch).grid(row=4, column=4)
+
+        Label(text='Encounter').grid(row=5, column=0)
+        Label(textvariable=self.encounter_text).grid(row=5, column=1)
+
+    def travel_watch(self):
+        if self.description_text.get() == 'Select a region':
+            showinfo('Error', 'Select a region first!')
+            return
+        self.encounter_check()
+
+    def activity_watch(self):
+        if self.description_text.get() == 'Select a region':
+            showinfo('Error', 'Select a region first!')
+            return
+        print('wee')
+
+    def sleep_watch(self):
+        if self.description_text.get() == 'Select a region':
+            showinfo('Error', 'Select a region first!')
+            return
+        print('wee')
+
+    def encounter_check(self):
+        encounter_chance = (int(self.encounter_chance_text.get()) + int(self.encounter_variable.get())) / 100
+        roll = random.random()
+        if roll < encounter_chance:
+            print(self.region_variable.get())
+            encounters = self.encounters_df.loc[self.encounters_df['Region'] == self.region_variable.get()]
+            print(encounters)
+        else:
+            self.encounter_text.set('No encounter')
 
     def select_region(self, selected):
         region_data = self.regions_df.loc[self.regions_df['Name'] == selected]
@@ -43,6 +92,8 @@ class Watch_Builder_GUI:
         self.foraging_text.set(foraging)
         navigation = region_data['Navigation'].to_string(index=False)
         self.navigation_text.set(navigation)
+        encounter_chance = region_data['Chance'].to_string(index=False)
+        self.encounter_chance_text.set(encounter_chance)
 
 
 root = Tk()
